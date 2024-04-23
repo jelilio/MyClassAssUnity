61c69727f7510db2dab541612cf4242173a2db70
Jelili Adesina <jelilio.adesina@gmail.com>
2024-04-23 17:13:07 +0100

    implemented mario jumping

---
+++ Assets/Scripts/SMB/Extensions.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace SMB
+{
+    public static class Extensions
+    {
+        private static LayerMask layerMask = LayerMask.GetMask("Default");
+        
+        public static bool Raycast(this Rigidbody2D rigidbody, Vector2 direction)
+        {
+            if (rigidbody.isKinematic) {
+                return false;
+            }
+
+            float radius = 0.25f;
+            float distance = 0.375f;
+
+            RaycastHit2D hit = Physics2D.CircleCast(rigidbody.position, radius, direction.normalized, distance, layerMask);
+            return hit.collider != null && hit.rigidbody != rigidbody;
+        }
+    }
+}
--- Assets/Scripts/SMB/PlayerMovement.cs
+++ Assets/Scripts/SMB/PlayerMovement.cs
@@ -10,24 +10,48 @@ namespace SMB
 
         private Vector2 velocity;
         private float inputAxis;
-        
-        public float moveSpeed = 8f;
 
+        public float moveSpeed = 8f;
+        public float maxJumpHeight = 5f;
+        public float maxJumpTime = 1f;
+        public float jumpForce => (2f * maxJumpHeight) / (maxJumpTime / 2f);
+        public float gravity => (-2f * maxJumpHeight) / Mathf.Pow(maxJumpTime / 2f, 2f);
+
+     
+        public bool grounded { get; private set; }
+        public bool jumping { get; private set; }
+        
         private void Awake()
         {
-            rigidbody = GetComponent<Rigidbody2D>();
             camera = Camera.main;
+            rigidbody = GetComponent<Rigidbody2D>();
+        }
+        
+        private void OnEnable()
+        {
+            rigidbody.isKinematic = false;
+            velocity = Vector2.zero;
+            jumping = false;
+        }
+
+        private void OnDisable()
+        {
+            rigidbody.isKinematic = true;
+            velocity = Vector2.zero;
+            jumping = false;
         }
 
         private void Update()
         {
             HorizontalMovement();
-        }
+            
+            grounded = rigidbody.Raycast(Vector2.down);
+            if (grounded)
+            {
+                GroundedMovement();
+            }
 
-        private void HorizontalMovement()
-        {
-            inputAxis = Input.GetAxis("Horizontal");
-            velocity.x = Mathf.MoveTowards(velocity.x, inputAxis * moveSpeed, moveSpeed + Time.deltaTime);
+            ApplyGravity();
         }
 
         private void FixedUpdate()
@@ -41,6 +65,37 @@ namespace SMB
             position.x = Mathf.Clamp(position.x, leftEdge.x + 0.5f, rightEdge.x - 0.5f);
             
             rigidbody.MovePosition(position);
+        }
+
+        private void HorizontalMovement()
+        {
+            inputAxis = Input.GetAxis("Horizontal");
+            velocity.x = Mathf.MoveTowards(velocity.x, inputAxis * moveSpeed, moveSpeed + Time.deltaTime);
+        }
+
+        private void GroundedMovement()
+        {
+            // prevent gravity from infinitly building up
+            velocity.y = Mathf.Max(velocity.y, 0f);
+            jumping = velocity.y > 0f;
+
+            // perform jump
+            if (Input.GetButtonDown("Jump"))
+            {
+                velocity.y = jumpForce;
+                jumping = true;
+            }
+        }
+
+        private void ApplyGravity()
+        {
+            // check if falling
+            bool falling = velocity.y < 0f || !Input.GetButton("Jump");
+            float multiplier = falling ? 2f : 1f;
+
+            // apply gravity and terminal velocity
+            velocity.y += gravity * multiplier * Time.deltaTime;
+            velocity.y = Mathf.Max(velocity.y, gravity / 2f);
         }
     }
 }
